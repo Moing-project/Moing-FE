ddcb75d3503700a3f133c1ab877e8b4af17c33e4
jwc406 <77850103+jwc406@users.noreply.github.com>
2023-08-05 19:05:26 +0900

    [modify] 서브도메인 분리, 워크스페이스 라우팅 제거

--- src/App.tsx
+++ src/App.tsx
@@ -3,43 +3,15 @@ import "./App.css";
 import Layout from "./layoutComponents/Layout";
 import Rending from "./pageComponents/Rending";
 import Main from "./pageComponents/Main";
-import MoingSpace from "./pageComponents/MoingSpace";
 import Projects from "./pageComponents/Projects";
-import MoingLayout from "./layoutComponents/MoingLayout";
-import Kanban from "./pageComponents/Kanban";
-import Issue from "./pageComponents/issue/Issue";
-import IssueDetail from "./pageComponents/issue/IssueDetail";
 
 function App() {
   return (
-    // <Routes>
-    //   <Route path="/" element={<Layout />}>
-    //     <Route index element={<Rending />} />
-    //     <Route path="main" element={<Main />} />
-    //     <Route path="projects" element={<Projects />} />
-    //   </Route>
-    //   {/* MoingSpace 컴포넌트를 루트로 설정 */}
-    //   <Route path="/" element={<MoingLayout />}>
-    //     {/* 이제 MoingSpace 컴포넌트 내부에 중첩된 라우팅을 구성합니다. */}
-    //     <Route path="moingSpace" element={<MoingSpace />} />
-    //     <Route path="kanban" element={<Kanban />} />
-    //     <Route path="issue" element={<Issue />}>
-    //       <Route path="detail" element={<IssueDetail />} />
-    //     </Route>
-    //   </Route>
-    // </Routes>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Rending />} />
         <Route path="main" element={<Main />} />
         <Route path="projects" element={<Projects />} />
-      </Route>
-      <Route path="/moingspace/*" element={<MoingLayout />}>
-        <Route index element={<MoingSpace />} />
-        <Route path="kanban" element={<Kanban />} />
-        <Route path="issue" element={<Issue />}>
-          <Route path="detail" element={<IssueDetail />} />
-        </Route>
       </Route>
     </Routes>
   );
--- src/pageComponents/MoingSpace.tsx
+++
@@ -1,3 +0,0 @@
-export default function MoingSpace() {
-  return <div>모잉 스페이스 메인입니다</div>;
-}
