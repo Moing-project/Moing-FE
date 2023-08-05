244e8eaf67609ff6888912ee68d4dbbf970f51fd
jwc406 <77850103+jwc406@users.noreply.github.com>
2023-08-05 19:18:34 +0900

    [modify] 레이아웃 컴포넌트 제거, Outlet 대신 App.tsx에 헤더, 푸터 추가하여 레이아웃 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Layout from "./layoutComponents/Layout";
+import Footer from "./layoutComponents/Footer";
+import Header from "./layoutComponents/Header";
 import Rending from "./pageComponents/Rending";
 import Main from "./pageComponents/Main";
 import Projects from "./pageComponents/Projects";
+import NotFound from "./pageComponents/NotFound";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Rending />} />
-        <Route path="main" element={<Main />} />
-        <Route path="projects" element={<Projects />} />
-      </Route>
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Rending />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/projects" element={<Projects />} />
+        {/* 상단에 위치하는 라우트들의 규칙 중 일치하는 라우트가 없다면 아래가 화면에 나타남 */}
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
