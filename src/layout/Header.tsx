7834290d43a5208a887c190203f9c4d9ce66145d
jwc406 <77850103+jwc406@users.noreply.github.com>
2023-08-11 18:03:45 +0900

    [add] 헤더, 로그인/회원가입 페이지 이동 추가
    #1 버튼 props, 타입에 onClick 추가
    #2 로그인/회원가입 페이지에 Link 추가

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,9 +1,19 @@
 import { PrimaryBtn } from "../components/Buttons";
 import * as S from "../styledComponents/Header";
 import { ReactComponent as Logo } from "../assets/images/logo.svg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
+
+  const handleSigninClick = () => {
+    navigate("/signin");
+  };
+
   return (
     <S.HeaderLayout>
       <S.HeaderBox>
@@ -12,6 +22,7 @@ export default function Header() {
         </Link>
         <div style={{ display: "flex", gap: "10px" }}>
           <PrimaryBtn
+            onClick={handleLoginClick}
             $shape="filled"
             $status="active"
             $width="short"
@@ -20,6 +31,7 @@ export default function Header() {
             로그인
           </PrimaryBtn>
           <PrimaryBtn
+            onClick={handleSigninClick}
             $status="active"
             $shape="solid"
             $width="short"
