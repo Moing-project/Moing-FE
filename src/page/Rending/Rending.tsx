8664c657e4bf2dc9b730d74de34f39944f1580b6
김지아 <wltkd0515@naver.com>
2023-08-14 21:25:28 +0900

    [add]회원가입,서비스이용약관 모달창

--- src/page/Rending/Rending.tsx
+++ src/page/Rending/Rending.tsx
@@ -1,49 +1,10 @@
 import RendingSection1 from './RendingSection1';
 import RendingSection2 from './RendingSection2';
 import RendingSection3 from './RendingSection3';
-import styled from 'styled-components';
+
 export default function Rending() {
-  // 각 세션별로 보여주기/ pr 하기전 지우기
   return (
     <div>
-      {/* <TestLayout>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-        <p>약관 테스트입니다 이것저것 넣어보자</p>
-      </TestLayout> */}
       <RendingSection1 />
       <RendingSection2 />
       <RendingSection3 />
@@ -51,9 +12,3 @@ export default function Rending() {
     </div>
   );
 }
-
-const TestLayout = styled.div`
-  padding: 15px;
-  height: 400px;
-  overflow-y: scroll;
-`;
