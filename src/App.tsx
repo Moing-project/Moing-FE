fbb6af5a588df3a50189cb771b4889e3c770f3e4
김지아 <wltkd0515@naver.com>
2023-08-24 03:53:58 +0900

    [modify]Footer 바닥고정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { Routes, Route } from "react-router-dom";
-import Footer from "./layout/Footer";
-import Header from "./layout/Header";
-import Rending from "./page/Rending/Rending";
-import Main from "./page/Main";
-import Projects from "./page/Projects/Projects";
-import NotFound from "./page/NotFound";
-import SignIn from "./page/SignIn/SignIn";
-import Login from "./page/Login/Login";
-import { MarginLayout } from "./styledComponents/commons/commonStyle";
-import SignInDone from "./page/SignInDone";
-import ProjectCreate from "./page/ProjectCreate/ProjectCreate";
-
+import { Routes, Route } from 'react-router-dom';
+import Footer from './layout/Footer';
+import Header from './layout/Header';
+import Rending from './page/Rending/Rending';
+import Main from './page/Main';
+import Projects from './page/Projects/Projects';
+import NotFound from './page/NotFound';
+import SignIn from './page/SignIn/SignIn';
+import Login from './page/Login/Login';
+import { MarginLayout } from './styledComponents/commons/commonStyle';
+import SignInDone from './page/SignInDone';
+import ProjectCreate from './page/ProjectCreate/ProjectCreate';
+import ProjectDetail from './page/ ProjectDetails/ProjectDetail';
 function App() {
   return (
     <>
@@ -21,9 +21,11 @@ function App() {
           <Route path="/main" element={<Main />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/projects/create" element={<ProjectCreate />} />
+          <Route path="/projects/:id" element={<ProjectDetail />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signin/done" element={<SignInDone />} />
           <Route path="/login" element={<Login />} />
+
           {/* 상단에 위치하는 라우트들의 규칙 중 일치하는 라우트가 없다면 아래가 화면에 나타남 */}
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
--- src/styledComponents/Footer.ts
+++ src/styledComponents/Footer.ts
@@ -1,21 +1,23 @@
-import { styled } from "styled-components";
+import { styled } from 'styled-components';
 
 const FooterLayout = styled.div`
-    width : 100%;
-    background-color : #f4f4f4;
-`
+  width: 100%;
+  background-color: #f4f4f4;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+`;
 
 const FooterContainer = styled.div`
-    padding : 36px 32px 95px 32px;
-`
+  padding: 36px 32px 95px 32px;
+`;
 
 const ListBox = styled.ol`
-    display : flex;
-    gap : 20px;
-    padding : 0px;
-    font-weight : 600;
-    margin-bottom : 50px;
-`
+  display: flex;
+  gap: 20px;
+  padding: 0px;
+  font-weight: 600;
+  margin-bottom: 50px;
+`;
 
-
-export {ListBox, FooterLayout, FooterContainer} 
+export { ListBox, FooterLayout, FooterContainer };
