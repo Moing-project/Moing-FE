b5e63e9a56d4eab10953ae2afaef40637c9bf3c2
김지아 <wltkd0515@naver.com>
2023-08-29 17:07:50 +0900

    [add]페이지네이션디자인

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import GlobalStyle from "./styledComponents/commons/GlobalStyle";
-import { Provider } from "react-redux";
-import store from "./redux/config/configStore";
-import "./styles/fonts.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import GlobalStyle from './styledComponents/commons/GlobalStyle';
+import { Provider } from 'react-redux';
+import store from './redux/config/configStore';
+import './styles/fonts.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
