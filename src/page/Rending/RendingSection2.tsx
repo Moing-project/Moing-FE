839150e39a2f00839647a22382b575be0aac4a38
jwc406 <77850103+jwc406@users.noreply.github.com>
2023-09-05 19:58:41 +0900

    update subdomain location

--- src/page/Rending/RendingSection2.tsx
+++ src/page/Rending/RendingSection2.tsx
@@ -31,7 +31,7 @@ function RendingSection2() {
                 $width="long"
                 $height="high"
                 onClick={() => {
-                  window.location.href = "www.moingspace.moingtool.com";
+                  window.location.href = "https://moingspace.moingtool.com";
                 }}
               >
                 모잉 둘러보기
